113bcb30c6e3338888bdd774e9c0126563656b78
Erdem <98175885+Erdem193@users.noreply.github.com>
2022-02-27 15:54:48 +0300

    Add files via upload

---
+++ 6.hafta_odev.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main() {
+  int durum;
+  srand(time(0));
+   for(int i=0;i<3;i++)
+ {
+  int sayi=rand()%9;
+  cout<<"Sayin "<<sayi<<" ise 1 degil ise 0 yaz"<<endl;
+  cin>>durum;
+    if(durum==1)
+  {
+   cout<<i+1<<". Denemede Buldum!";
+   break;
+  }
+ }
+   if (durum==0)cout<<"Malesef Bulamadim!";
+  return 0;
+}
